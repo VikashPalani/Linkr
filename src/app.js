48eeb48c17cfb318d677b95831e94bbe482e8a8b
VikashPalani <vikash1702palani@gmail.com>
2024-09-29 17:13:21 +0530

    APIs for basic CRUD operations

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+//API Examples for CRUD operations (Basic and important operations in any application) 
+
 //Middleware to convert JSON data to JavaScript object
 app.use(express.json());
 
@@ -27,6 +29,61 @@ app.post("/signup" , async(req,res) => {
     } catch(err){
         console.error(err);
         res.status(400).send("Error occurred while creating user");
+    }
+});
+
+//Get user by emailId
+app.get("/user", async(req,res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId: userEmail});
+        if(users.length === 0){
+            res.statusMessage(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("Error occurred while fetching user");
+    }
+});
+
+//GET all the users from the database
+app.get("/feed", async(req,res) => {
+    try{
+        //While passing an empty object to find() method, it will return all the documents from the collection
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Error occurred while fetching users");
+    }
+});
+
+//Delete user by userId
+app.delete("/user", async(req,res) => {
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("Error occurred while deleting user");
+    }
+});
+
+//Update user by userId
+app.patch("/user", async (req,res) => {
+    const userId = req.body.userId;
+    const updatedData = req.body;
+    try{
+        await User.findByIdAndUpdate({_id:userId}, updatedData);
+        res.send("User updated successfully");
+    }
+    catch(err){
+        res.status(400).send("Error occurred while updating user");
     }
 });
 
