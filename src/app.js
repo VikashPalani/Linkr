63d002810191cbf59f4ba306decf09de9a83860c
VikashPalani <vikash1702palani@gmail.com>
2024-09-30 11:07:28 +0530

    Password encryption and validation

--- src/app.js
+++ src/app.js
@@ -2,35 +2,67 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+const {validateSignUpData} = require("./utils/validation");
 
 //API Examples for CRUD operations (Basic and important operations in any application) 
 
 //Middleware to convert JSON data to JavaScript object
 app.use(express.json());
 
-//Connect to Database first before starting the server [IMPORTANT]
-
+//Create a new user
 app.post("/signup" , async(req,res) => {
 
-    //Creating a new INSTANCE of User model
-    // const user = new User({
-    //     firstName: "Thala",
-    //     lastName: "Dhoni",
-    //     emailId: "dhoni@gmail.com",
-    //     password: "dhoni@123",
-    // });
-
-    //Creating a new INSTANCE of User model using req.body (All the data will be sent through API request)
-    const user = new User(req.body);
-
     try{
+        //Validation of data
+        validateSignUpData(req);
+
+        const {firstName, lastName, emailId, password} = req.body;
+
+        //Encrypt the password before saving it to the database
+        const passwordHash = await bcrypt.hash(password,10);
+
+        //Creating a new INSTANCE of User model (All the data will be sent through API request)
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
+
         await user.save();
         res.send("User created successfully");
-    } catch(err){
-        console.error(err);
-        res.status(400).send("Error occurred while creating user");
+    } 
+    catch(err){
+        res.status(400).send("ERROR: " + err.message);
     }
+
 });
+
+//Login user
+app.post("/login", async(req,res) => {
+    try{
+        const {emailId,password} = req.body;
+        //Write email validation here
+        const user = await User.findOne({emailId: emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        //returns true if the password is valid
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(isPasswordValid){
+            res.send("User logged in successfully");
+        }
+        else{
+            throw new Error("Invalid credentials");
+        }
+    }
+    catch(err){
+        res.status(400).send("ERROR: " + err.message);
+    }
+})
 
 //Get user by emailId
 app.get("/user", async(req,res) => {
@@ -79,14 +111,34 @@ app.patch("/user", async (req,res) => {
     const userId = req.body.userId;
     const updatedData = req.body;
     try{
-        await User.findByIdAndUpdate({_id:userId}, updatedData);
+        const ALLOWED_UPDATES = ["userId","age","gender","password","skills"];
+
+        //Checking if the user is trying to update any field other than the allowed fields
+        const isUpdateAllowed = Object.keys(updatedData).every((update) => 
+            ALLOWED_UPDATES.includes(update)
+        );
+
+        if(!isUpdateAllowed){
+            throw new Error("Invalid updates");
+        }
+
+        if(updatedData?.skills.length > 10){
+            throw new Error("Skills cannot be more than 10");
+        }
+
+        const user = await User.findByIdAndUpdate({_id:userId}, updatedData, {
+            returnDocument: "after",
+            //This will run the validations specified in the schema while updating the document
+            runValidators: true,
+        });
         res.send("User updated successfully");
     }
     catch(err){
-        res.status(400).send("Error occurred while updating user");
+        res.status(400).send("Updates failed" + err.message);
     }
 });
 
+//Connect to Database first before starting the server [IMPORTANT]
 connectDB()
     .then(() => {
         console.log("Database connected successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,57 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 // User Schema
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
     },
     lastName: {
         type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 20,
     },
     emailId: {
         type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid" + value);
+            }
+        },
     },
     password: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
     },
     gender: {
         type: String,
-    }
-});
+
+        //We can use Validate property to customize the validations for the fields
+        // This will only run when we create a new document and not when we update the existing document unless we specify the runValidators option in API
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender is invalid");
+            }
+        },
+    },
+    skills: {
+        type: [String],
+    },
+},
+//This will create two fields in the document createdAt and updatedAt);
+{timestamps: true}
+);
 
 // User Model | Here 'User' is the name of the model
 module.exports = mongoose.model("User", userSchema);
